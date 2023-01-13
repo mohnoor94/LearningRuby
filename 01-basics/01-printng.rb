68134d0c25f201654700a9ed54cd5d71517a4dca
mohnoor94 <moh.noor94@gmail.com>
2023-01-13 22:37:27 +0300

    puts and print

---
+++ 01-basics/01-printng.rb
@@ -0,0 +1,26 @@
+# The `puts` method prints a value to the console, followed by a new line.
+
+puts "Hello World"
+puts 1 + 2
+puts 1.0 / 2.0
+puts # prints a blank line
+puts "After the blank line"
+puts "Hello" + " " + "World"
+puts 1, "Hello", 3 # prints each value on a separate line
+
+puts "Hello\nWorld" # prints "Hello" on one line and "World" on another line
+
+puts 'Hello World' # single quotes are also used to create strings
+puts 'Hello "World"'
+puts "Hello \"World\""
+
+puts "Hello\tWorld" # prints "Hello" followed by a tab character and then "World"
+
+
+########################################################################################
+
+# The `print` method prints a value to the console, without a new line.
+
+print "Hello World"
+print 1 + 2
+print 1.0 / 2.0
