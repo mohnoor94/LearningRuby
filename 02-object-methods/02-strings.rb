1af5e701c8ac36b43b5ded1217ad92a083ca3360
mohnoor94 <moh.noor94@gmail.com>
2023-01-17 23:59:45 +0300

    Strings & Numbers

--- 02-object-methods/02-strings.rb
+++ 02-object-methods/02-strings.rb
@@ -28,12 +28,38 @@ puts "The result of 1 + 1 is #{1 + 1}." # "The result of 1 + 1 is 2."
 # The `chomp` method removes the newline character `\n` from the end of the string.
 
 puts "What is your name?"
-name = gets.chomp
+## Uncomment the next line to run the code.
+# name = gets.chomp
 
 # Note: The `gets` method returns a string. We need to convert it to an integer.
 puts "Great! What is your age?"
-age = gets.chomp.to_i # Convert the string to an integer.
+## Uncomment the next line to run the code.
+# age = gets.chomp.to_i # Convert the string to an integer.
 
 puts "Hello #{name}. You are #{age} years old. Wow!"
 
 ########################################################################################
+
+# Convert a String to an Integer
+
+# The `to_i` method converts a string to an integer.
+# If the string cannot be converted to an integer, it returns 0.
+
+puts "1".to_i # 1
+puts "1.5".to_i # 1
+puts "one".to_i # 0
+puts "one".to_i.class # Integer - .class returns the class of the object
+
+# The `to_f` method converts a string to a float.
+# If the string cannot be converted to a float, it returns 0.0.
+
+puts "1".to_f # 1.0
+puts "1.5".to_f # 1.5
+puts "one".to_f # 0.0
+puts "one".to_f.class # Float - .class returns the class of the object
+
+# The `to_s` method converts an integer or float to a string.
+
+puts 1.to_s # "1"
+puts 1.5.to_s # "1.5"
+puts 1.to_s.class # String - .class returns the
